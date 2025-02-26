b88f6bc95603b24aafd4ae4635d52235afffdc3d
Danny <jordantsolov@gmail.com>
2025-02-26 14:49:17 +0200

    iditial commit

---
+++ src/test/java/com/telerik/virtualwallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerik.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
