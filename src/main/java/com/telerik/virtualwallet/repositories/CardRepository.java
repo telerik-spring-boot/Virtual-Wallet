416198832a9666cf15a72d0858b7e3c95dd2becb
nPilashev <n.a.pilashev@gmail.com>
2025-02-26 21:07:15 +0000

    Add Card Repository

---
+++ src/main/java/com/telerik/virtualwallet/repositories/CardRepository.java
@@ -0,0 +1,22 @@
+package com.telerik.virtualwallet.repositories;
+
+import com.telerik.virtualwallet.models.Card;
+
+import java.util.List;
+
+public interface CardRepository {
+
+    List<Card> getAllCards();
+
+    List<Card> getCardsByUserId(int userId);
+
+    Card getCardById(int id);
+
+    void addCard(Card card);
+
+    void updateCard(Card card);
+
+    void deleteCard(int cardId);
+
+
+}
---
+++ src/main/java/com/telerik/virtualwallet/repositories/CardRepositoryImpl.java
@@ -0,0 +1,78 @@
+package com.telerik.virtualwallet.repositories;
+
+import com.telerik.virtualwallet.models.Card;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CardRepositoryImpl implements CardRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public CardRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Card> getAllCards() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Card> query = session.createQuery("from Card", Card.class);
+            return query.list();
+        }
+    }
+
+    @Override
+    public List<Card> getCardsByUserId(int userId) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Card> query = session.createQuery("from Card where user.id = :userId", Card.class);
+            query.setParameter("userId", userId);
+            return query.list();
+        }
+    }
+
+    @Override
+    public Card getCardById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Card.class, id);
+        }
+    }
+
+    @Override
+    public void addCard(Card card) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(card);
+            session.getTransaction().commit();
+        }
+
+    }
+
+    @Override
+    public void updateCard(Card card) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(card);
+            session.getTransaction().commit();
+        }
+
+    }
+
+    @Override
+    public void deleteCard(int cardId) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            Card card = session.get(Card.class, cardId);
+            session.remove(card);
+
+            session.getTransaction().commit();
+        }
+
+    }
+}
