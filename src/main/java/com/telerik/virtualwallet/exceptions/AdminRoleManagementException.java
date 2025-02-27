ca32b69c0b5b2130b864a586fe6bb9ad88f03c1c
Danny <jordantsolov@gmail.com>
2025-02-27 21:36:55 +0200

    add AdminRoleManagementException

---
+++ src/main/java/com/telerik/virtualwallet/exceptions/AdminRoleManagementException.java
@@ -0,0 +1,7 @@
+package com.telerik.virtualwallet.exceptions;
+
+public class AdminRoleManagementException extends RuntimeException {
+    public AdminRoleManagementException(String message) {
+        super(message);
+    }
+}
