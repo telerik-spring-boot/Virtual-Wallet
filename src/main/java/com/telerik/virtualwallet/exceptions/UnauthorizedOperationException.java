75e0ca4595acb1234be60c7f1c4e4816a36102eb
Danny <jordantsolov@gmail.com>
2025-02-27 16:04:10 +0200

    add UnauthorizedOperationException

---
+++ src/main/java/com/telerik/virtualwallet/exceptions/UnauthorizedOperationException.java
@@ -0,0 +1,8 @@
+package com.telerik.virtualwallet.exceptions;
+
+public class UnauthorizedOperationException extends RuntimeException {
+
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
