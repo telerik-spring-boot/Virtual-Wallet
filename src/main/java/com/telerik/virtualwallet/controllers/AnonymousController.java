0610bb3ff505a5831ee36d4c9b3b7cf1f9c8779f
Danny <jordantsolov@gmail.com>
2025-03-01 18:45:27 +0200

    add register functionality

--- src/main/java/com/telerik/virtualwallet/controllers/AnonymousController.java
+++ src/main/java/com/telerik/virtualwallet/controllers/AnonymousController.java
@@ -1,16 +1,22 @@
 package com.telerik.virtualwallet.controllers;
 
+import com.telerik.virtualwallet.helpers.UserMapper;
+import com.telerik.virtualwallet.models.User;
 import com.telerik.virtualwallet.models.dtos.LoginDTO;
+import com.telerik.virtualwallet.models.dtos.RegisterDTO;
 import com.telerik.virtualwallet.services.jwt.JwtService;
+import com.telerik.virtualwallet.services.user.UserService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,12 +32,16 @@ public class AnonymousController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
     @Autowired
-    public AnonymousController(AuthenticationManager authenticationManager, JwtService jwtService, UserDetailsService userDetailsService) {
+    public AnonymousController(AuthenticationManager authenticationManager, JwtService jwtService, UserDetailsService userDetailsService, UserService userService, UserMapper userMapper) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @PostMapping("/login")
@@ -48,5 +58,15 @@ public class AnonymousController {
         response.put("token", jwtToken);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody @Valid RegisterDTO registerDTO) {
+
+        User user = userMapper.dtoToUser(registerDTO);
+
+        userService.create(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
