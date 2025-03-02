6b187598d022d78cfc28a0f3dd742465ebac0989
Danny <jordantsolov@gmail.com>
2025-03-02 12:59:51 +0200

    add picture upload exception

---
+++ src/main/java/com/telerik/virtualwallet/exceptions/PictureUploadException.java
@@ -0,0 +1,7 @@
+package com.telerik.virtualwallet.exceptions;
+
+public class PictureUploadException extends RuntimeException {
+    public PictureUploadException(String message) {
+        super(message);
+    }
+}
