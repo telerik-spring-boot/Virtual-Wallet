15221a41d9367e49c16bfb2c7359a273c795c822
Danny <jordantsolov@gmail.com>
2025-03-02 17:33:40 +0200

    add stock display dto

---
+++ src/main/java/com/telerik/virtualwallet/models/dtos/StockDisplayDTO.java
@@ -0,0 +1,58 @@
+package com.telerik.virtualwallet.models.dtos;
+
+
+import java.time.LocalDateTime;
+
+public class StockDisplayDTO {
+
+    private LocalDateTime purchasedAt;
+
+    private String stockSymbol;
+
+    private double quantity;
+
+    private double price;
+
+
+    public StockDisplayDTO() {
+    }
+
+    public StockDisplayDTO(LocalDateTime purchasedAt, String stockSymbol, double quantity, double price) {
+        this.purchasedAt = purchasedAt;
+        this.stockSymbol = stockSymbol;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public LocalDateTime getPurchasedAt() {
+        return purchasedAt;
+    }
+
+    public void setPurchasedAt(LocalDateTime purchasedAt) {
+        this.purchasedAt = purchasedAt;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public void setStockSymbol(String stockSymbol) {
+        this.stockSymbol = stockSymbol;
+    }
+}
