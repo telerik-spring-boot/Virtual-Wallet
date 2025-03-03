ba3f5fb80d83b962bab5a21af7525c4e911030d4
Danny <jordantsolov@gmail.com>
2025-03-03 22:15:11 +0200

    add user service tests

---
+++ src/test/java/com/telerik/virtualwallet/DummyObjectProvider.java
@@ -0,0 +1,37 @@
+package com.telerik.virtualwallet;
+
+import com.telerik.virtualwallet.models.Role;
+import com.telerik.virtualwallet.models.User;
+
+
+public class DummyObjectProvider {
+
+
+    public static User createMockUser(){
+        User user = new User();
+
+        user.setId(1);
+        user.setEmail("george.bush@gmail.com");
+        user.setUsername("george_bush");
+        user.setPassword("Password123!");
+        user.setBlocked(false);
+        user.setPhoneNumber("+0123456789");
+
+        return user;
+    }
+
+
+    public static User createMockAdmin(){
+        User user = new User();
+
+        user.setId(2);
+        user.setEmail("george.bush2@gmail.com");
+        user.setUsername("george_bush2");
+        user.setPassword("Password123!");
+        user.setBlocked(false);
+        user.setPhoneNumber("+0123456785");
+        user.getRoles().add(new Role("ADMIN"));
+
+        return user;
+    }
+}
