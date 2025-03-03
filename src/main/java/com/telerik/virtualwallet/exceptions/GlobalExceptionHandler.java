b0e2737dc26b338b5c40f73d627b96f771500689
nPilashev <n.a.pilashev@gmail.com>
2025-03-03 16:18:03 +0000

    Add Incompatible Currencies Exception to Global Exception Handler

--- src/main/java/com/telerik/virtualwallet/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/telerik/virtualwallet/exceptions/GlobalExceptionHandler.java
@@ -28,6 +28,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(IncompatibleCurrenciesException.class)
+    public ResponseEntity<?> handleIncompatibleCurrenciesException(IncompatibleCurrenciesException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
     @ExceptionHandler(AdminRoleManagementException.class)
     public ResponseEntity<?> handleAdminRoleManagementException(AdminRoleManagementException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
