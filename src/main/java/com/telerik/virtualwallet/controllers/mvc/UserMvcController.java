e4aec2c281ed0970ab9ce2e036fa7c8f66ab7b38
Danny <jordantsolov@gmail.com>
2025-03-06 17:31:44 +0200

    add layout template

--- src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
+++ src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
@@ -8,8 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UserMvcController {
 
-    @GetMapping("me")
+    @GetMapping("/dashboard")
     public String getOverview(){
         return "index";
+    }
+
+    @GetMapping("/balance")
+    public String getCards(){
+        return "balance";
+    }
+
+    @GetMapping("/cards")
+    public String getBalance(){
+        return "card";
+    }
+
+    @GetMapping("/recipients")
+    public String getRecipients(){
+        return "recipients";
+    }
+    @GetMapping("/settings")
+    public String getSettings(){
+        return "settings";
+    }
+    @GetMapping("/transactions")
+    public String getTransactions(){
+        return "transaction";
     }
 }
