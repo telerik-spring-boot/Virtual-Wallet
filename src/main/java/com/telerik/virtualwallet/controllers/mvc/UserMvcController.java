d3db706eb24f5913c60de6d02d53dd76afad2468
Danny <jordantsolov@gmail.com>
2025-03-07 16:15:33 +0200

    fix template backgrounds + dummy test for admin panel

--- src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
+++ src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
@@ -1,12 +1,36 @@
 package com.telerik.virtualwallet.controllers.mvc;
 
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserMvcController {
+
+    @ModelAttribute("isAdmin")
+    public boolean populateIsAdmin(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        List<String> roles = new ArrayList<>();
+        if (authentication != null && authentication.isAuthenticated()) {
+            roles =  authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toList();
+        }
+
+        return roles.contains("ROLE_ADMIN");
+
+    }
 
     @GetMapping("/dashboard")
     public String getOverview(){
@@ -27,6 +51,7 @@ public class UserMvcController {
     public String getRecipients(){
         return "recipients";
     }
+
     @GetMapping("/settings")
     public String getSettings(){
         return "settings";
@@ -50,5 +75,12 @@ public class UserMvcController {
     @GetMapping("/register")
     public String getRegister(){
         return "register";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getAdminPanel(){
+
+        return "admin-panel";
     }
 }
