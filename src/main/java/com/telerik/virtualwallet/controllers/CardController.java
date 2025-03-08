2f7a1c435d29efa05688decc487a6778a4b1e754
nPilashev <n.a.pilashev@gmail.com>
2025-03-08 14:16:02 +0000

    Move add, update and delete card methods in Card controller from User controller

--- src/main/java/com/telerik/virtualwallet/controllers/CardController.java
+++ src/main/java/com/telerik/virtualwallet/controllers/CardController.java
@@ -2,14 +2,14 @@ package com.telerik.virtualwallet.controllers;
 
 import com.telerik.virtualwallet.helpers.CardMapper;
 import com.telerik.virtualwallet.models.Card;
+import com.telerik.virtualwallet.models.dtos.card.CardCreateDTO;
 import com.telerik.virtualwallet.models.dtos.card.CardDisplayDTO;
 import com.telerik.virtualwallet.services.card.CardService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -33,6 +33,39 @@ public class CardController {
 
         return ResponseEntity.ok(cardMapper.cardToCardDisplayDTO(card));
 
+    }
+
+    @PostMapping()
+    public ResponseEntity<CardDisplayDTO> addCardToUserByUsername(@RequestBody CardCreateDTO dto) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        Card cardToAdd = cardMapper.createDtoToCard(dto);
+        cardService.addCard(auth.getName(), cardToAdd);
+
+        return ResponseEntity.ok(cardMapper.cardToCardDisplayDTO(cardService.getCardById(cardToAdd.getId())));
+
+    }
+
+    @PutMapping("/{cardId}")
+    @PreAuthorize("@cardSecurityService.isUserCardHolder(#cardId, authentication.name)")
+    public ResponseEntity<CardDisplayDTO> updateCardFromUser(@PathVariable int cardId,
+                                                             @RequestBody CardCreateDTO dto) {
+
+        Card card = cardMapper.createDtoToCard(cardId, dto);
+
+        cardService.updateCard(card);
+
+        return ResponseEntity.ok(cardMapper.cardToCardDisplayDTO(cardService.getCardById(cardId)));
+    }
+
+    @DeleteMapping("/{cardId}")
+    @PreAuthorize("@cardSecurityService.isUserCardHolder(#cardId, authentication.name) ")
+    public ResponseEntity<String> removeCardFromUser(@PathVariable int cardId) {
+
+        cardService.deleteCard(cardId);
+
+        return ResponseEntity.ok(String.format("Card with id %d successfully removed.", cardId));
     }
 
 }
