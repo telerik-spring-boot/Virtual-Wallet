b3df3f51763c2e1cf46ce39326adc5d3cbb6cadb
nPilashev <n.a.pilashev@gmail.com>
2025-03-09 14:39:20 +0000

    Add getMethods in TransferRepository with Filters, pageable and sorting

--- src/main/java/com/telerik/virtualwallet/repositories/transaction/TransferRepository.java
+++ src/main/java/com/telerik/virtualwallet/repositories/transaction/TransferRepository.java
@@ -1,16 +1,19 @@
 package com.telerik.virtualwallet.repositories.transaction;
 
-import com.telerik.virtualwallet.models.Transaction;
 import com.telerik.virtualwallet.models.Transfer;
-import com.telerik.virtualwallet.models.filters.FilterTransactionsOptions;
+import com.telerik.virtualwallet.models.filters.FilterTransferOptions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface TransferRepository {
 
-    Page<Transfer> getAllTransfers(FilterTransactionsOptions options, Pageable pageable);
+    Page<Transfer> getAllTransfers(FilterTransferOptions options, Pageable pageable);
 
-    Page<Transfer> getAllTransfersByUsername(FilterTransactionsOptions options, Pageable pageable, String username);
+    Page<Transfer> getAllTransfersByUsername(FilterTransferOptions options, Pageable pageable, String username);
+
+    Page<Transfer> getAllTransfersByWalletId(FilterTransferOptions options, Pageable pageable, int walletId);
+
+    Transfer getTransferById(int id);
 
     void createTransfer(Transfer transfer);
 }
