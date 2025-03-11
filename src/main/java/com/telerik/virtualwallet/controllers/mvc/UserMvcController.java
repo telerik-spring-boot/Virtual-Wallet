f5365f56b4fdb93d58b00fedb890bf64ba830e16
Danny <jordantsolov@gmail.com>
2025-03-11 16:25:13 +0200

    add thymeleaf to settings view + functionality for edit + delete

--- src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
+++ src/main/java/com/telerik/virtualwallet/controllers/mvc/UserMvcController.java
@@ -1,21 +1,49 @@
 package com.telerik.virtualwallet.controllers.mvc;
 
 
+import com.telerik.virtualwallet.exceptions.DuplicateEntityException;
+import com.telerik.virtualwallet.exceptions.EntityNotFoundException;
+import com.telerik.virtualwallet.exceptions.UnauthorizedOperationException;
+import com.telerik.virtualwallet.helpers.UserMapper;
+import com.telerik.virtualwallet.models.User;
+import com.telerik.virtualwallet.models.dtos.user.UserDisplayMvcDTO;
+import com.telerik.virtualwallet.models.dtos.user.UserUpdateDTO;
+import com.telerik.virtualwallet.models.dtos.user.UserUpdateMvcDTO;
+import com.telerik.virtualwallet.services.jwt.JwtService;
+import com.telerik.virtualwallet.services.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @Controller
 @RequestMapping("/users")
 public class UserMvcController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+    private final JwtService jwtService;
+
+    public UserMvcController(UserService userService, UserMapper userMapper, JwtService jwtService){
+        this.userService = userService;
+        this.userMapper = userMapper;
+        this.jwtService = jwtService;
+    }
 
     @ModelAttribute("isAdmin")
     public boolean populateIsAdmin(){
@@ -53,8 +81,62 @@ public class UserMvcController {
     }
 
     @GetMapping("/settings")
-    public String getSettings(){
-        return "settings";
+    public String showUserSettings(Authentication authentication, Model model, HttpServletRequest request) {
+
+        try {
+            User user = userService.getByUsername(authentication.getName());
+
+            UserUpdateMvcDTO userUpdate = userMapper.userToUserUpdateMvcDto(user);
+            UserDisplayMvcDTO userDisplay = userMapper.userToUserDisplayMvcDTO(user);
+
+
+            model.addAttribute("userUpdate", userUpdate);
+            model.addAttribute("userDisplay",userDisplay);
+            model.addAttribute("requestURI", request.getRequestURI());
+            model.addAttribute("token", jwtService.generateEmailVerificationToken(user.getEmail()));
+
+            return "settings";
+        } catch (UnauthorizedOperationException e) {
+            return "redirect:/auth/login";
+        }
+    }
+
+    @PutMapping("/settings")
+    public String handleUpdateUser(Authentication authentication, @Valid @ModelAttribute("userUpdate") UserUpdateMvcDTO userUpdateMvcDTO,  BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/users/settings";
+        }
+
+        try {
+
+            User userToUpdate = userMapper.dtoToUser(userUpdateMvcDTO, authentication.getName());
+
+            userService.update(userToUpdate);
+
+            redirectAttributes.addFlashAttribute("successUpdate", true);
+
+            return "redirect:/users/settings";
+
+        } catch (DuplicateEntityException e) {
+            bindingResult.reject("duplicateEntityError", e.getMessage());
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/users/settings";
+        }
+    }
+
+    @DeleteMapping
+    public String deleteUser(HttpServletRequest request, HttpServletResponse response, Authentication authentication, RedirectAttributes redirectAttributes){
+
+        userService.delete(authentication.getName());
+
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
+        redirectAttributes.addFlashAttribute("userDeleted", true);
+        return "redirect:/auth/login";
+
     }
 
     @GetMapping("/transactions")
--- src/main/java/com/telerik/virtualwallet/models/dtos/user/UserUpdateMvcDTO.java
+++ src/main/java/com/telerik/virtualwallet/models/dtos/user/UserUpdateMvcDTO.java
@@ -3,10 +3,11 @@ package com.telerik.virtualwallet.models.dtos.user;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public class UserUpdateDTO {
+public class UserUpdateMvcDTO {
 
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,20}$", message = "Password must contain at least one uppercase letter, one lowercase letter, one number, one symbol, and be between 8 to 20 characters long.")
-    private String password;
+    @Size(min=2, max =20, message = "Full name must be between 2 and 20 characters.")
+    @Pattern(regexp = "^[A-Za-z]+( [A-Za-z]+)*$", message = "Full name must contain only letters and spaces (one space between each word).")
+    private String fullName;
 
     @Size(min = 5, max = 254, message = "Email must be between 5 and 254 symbols.")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Email must be valid.")
@@ -15,21 +16,20 @@ public class UserUpdateDTO {
     @Pattern(regexp = "^\\+\\d{10}$", message = "Phone number must start with '+' followed by exactly 10 digits.")
     private String phoneNumber;
 
-    public UserUpdateDTO() {
-    }
+    public UserUpdateMvcDTO(){}
 
-    public UserUpdateDTO(String password, String phoneNumber, String emailAddress) {
-        this.password = password;
-        this.phoneNumber = phoneNumber;
+    public UserUpdateMvcDTO(String fullName, String emailAddress, String phoneNumber){
         this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.fullName = fullName;
     }
 
-    public String getPassword() {
-        return password;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getPhoneNumber() {
