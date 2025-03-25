0bcf08341e52444b94c4a54e12ca0c2b72dc31fe
Niki <n.a.pilashev@gmail.com>
2025-03-25 17:23:12 +0200

    Add home page

---
+++ src/main/java/com/telerik/virtualwallet/controllers/mvc/HomeMvcController.java
@@ -0,0 +1,25 @@
+package com.telerik.virtualwallet.controllers.mvc;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ui/home")
+public class HomeMvcController {
+
+    @GetMapping()
+    public String getHome(Authentication authentication, Model model) {
+        if(authentication != null){
+            model.addAttribute("isLoggedIn", true);
+        }
+
+        return "home";
+    }
+
+
+
+
+}
